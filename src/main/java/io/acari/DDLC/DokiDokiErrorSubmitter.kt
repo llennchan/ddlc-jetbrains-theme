29e21c52a6d4f8c75cb39ca82b36ecdaf32fe7ca
DATBOI <alexsimons9999@gmail.com>
2018-12-13 17:45:36 -0600

    New error submission class

---
+++ src/main/java/io/acari/DDLC/DokiDokiErrorSubmitter.kt
@@ -0,0 +1,10 @@
+package io.acari.DDLC
+
+import com.intellij.openapi.diagnostic.ErrorReportSubmitter
+
+class DokiDokiErrorSubmitter: ErrorReportSubmitter() {
+  override fun getReportActionText(): String {
+    return "R͠͡e̶͘pǫŗţ ̨̀̕t̡͝o̡͜҉ ͘I̶̛̛s̸̛s̛̕úe͘.͠"
+  }
+
+}
