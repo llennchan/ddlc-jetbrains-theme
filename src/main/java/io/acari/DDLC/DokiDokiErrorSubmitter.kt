adeef5a290effadb9b9fdd83ac38ed4eb152e26d
DATBOI <alexsimons9999@gmail.com>
2018-12-13 18:10:01 -0600

    Just a bit more readable.

--- src/main/java/io/acari/DDLC/DokiDokiErrorSubmitter.kt
+++ src/main/java/io/acari/DDLC/DokiDokiErrorSubmitter.kt
@@ -4,7 +4,7 @@ import com.intellij.openapi.diagnostic.ErrorReportSubmitter
 
 class DokiDokiErrorSubmitter: ErrorReportSubmitter() {
   override fun getReportActionText(): String {
-    return "R͠͡e̶͘pǫŗţ ̨̀̕t̡͝o̡͜҉ ͘I̶̛̛s̸̛s̛̕úe͘.͠"
+    return "Report͘Ī̷͙͠s̵͍̻̽̄s̶͔̯͐͊u̸̻̖͝e̷͓͍͌"
   }
 
 }
